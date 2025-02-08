f264ef24f8d7f69e34679cb012fbe8e65d974378
V4Cmeister <nikolaussoukopf@gmail.com>
2025-02-08 23:05:59 +0100

    main streamlit page

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Mechanism Simulator")
+
+st.write("Here you can simulate a mechanism by defining its configuration and then running the simulation.")
+st.write("Hier kann sich später ausgetobt werden...")
